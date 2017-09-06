b858a5cca3ad2e097cce3a7f5bb0a81e52f0513c
Matthias Hinz <matthias.m.hinz@gmail.com>
2017-09-06 14:57:37 +0200

    Init as R project Added table

---
+++ R/createTable.R
@@ -0,0 +1,19 @@
+library(htmltools)
+library(htmlwidgets)
+
+portale <- read.csv("data/portale_geocoded.csv")
+
+portale$Link <- paste0("<a href=\"",htmlEscape(portale$URL),"\" target=\"_blank\">",htmlEscape(portale$Titel),"</a>")
+
+tabledt <- DT::datatable(portale[c("Link","Beschreibung","Ort","Bezug","Lizenz", "Staatlich_Öffentlich")], escape = FALSE, colnames = c("Link","Beschreibung","Ort","Bezug","Lizenz", "Staatlich/Öffentlich"), options = list(
+  bPaginate = TRUE
+))
+
+
+#m <- prependContent(m, tags$meta(`http-equiv`="expires", content="0"))
+#m <- attachDependencies(m, tags$meta(`http-equiv`="expires", content="0"))
+#m$dependencies <- append(m$dependencies, tagList(tags$meta(`http-equiv`="expires", content="0")))
+setwd("out")
+saveWidget(tabledt, file="portale_tabelle.html",  selfcontained = FALSE)
+setwd("..")
+#saveWidget(m, file="portale.html", selfcontained = FALSE)
---
+++ R/create_map_function.R
@@ -0,0 +1,52 @@
+createMap <- function(portale) {
+  categories <- c("international","national","regional","kommunal")
+  colorlf <- c("green", "yellow", "blue", "brown")
+  names(colorlf) <- categories
+  
+  m <-
+    leaflet(data = portale) %>% addProviderTiles(providers$Stamen.TonerBackground) %>% addLegend(
+      colors = colorlf,
+      values = categories,
+      labels = categories,
+      title = "Open Data Portale"
+    )
+  
+  
+  
+  sapply(categories, function(category) {
+    group <- portale[portale$Bezug == category,]
+    cf <- paste0(
+      "function (cluster) {
+      var childCount = cluster.getChildCount();",
+      "var c = '",
+      colorlf[[category]],
+      "';",
+      "return new L.DivIcon({ html: '<div style=\"background-color:'+c+'\"><span>' + childCount + '</span></div>', className: 'marker-cluster', iconSize: new L.Point(40, 40) });
+  }"
+    )
+    
+    m <<-
+      addCircleMarkers(
+        m,
+        group$lon,
+        group$lat,
+        popup = group$popup,
+        group = category,
+        color =  colorlf[[category]],
+        label = htmlEscape(group$Titel),
+        clusterOptions = markerClusterOptions(iconCreateFunction = JS(cf)),
+        clusterId = category,
+        labelOptions = labelOptions()
+      )
+    # m <<- addCircleMarkers(m, group$lon, group$lat, popup = group$popup, group = category, color =  colormarker[[category]], label = group$Titel)
+    #m <<- addAwesomeMarkers(m, group$lon, group$lat, popup = group$popup, group = category, label = group$Titel)
+    
+    invisible()
+  })
+  
+  m <-
+    addLayersControl(m,
+                     overlayGroups = levels(portale$Bezug),
+                     options = layersControlOptions())
+  return(m)
+}
