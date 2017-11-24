34a48072080d89300fb09db5d8bf91a979a4727a
matthiashinz <matthias.hinz@uni-rostock.de>
2017-11-24 17:21:04 +0100

    improved server config

--- startshiny.sh
+++ startshiny.sh
@@ -2,6 +2,7 @@
 docker run --rm -d -p 85:80 \
 	-v $(pwd):/srv/shiny-server \
 	-v $(pwd)/shiny-portals/shinylog:/var/log/shiny-server/ \
+	-v $(pwd)/shiny-portals/shiny-server.conf:/etc/shiny-server/shiny-server.conf \
 	--name ogeportal \
 	-u `id -u $USER` \
 	shinyapp
