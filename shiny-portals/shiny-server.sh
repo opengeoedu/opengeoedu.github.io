d977bb434570f762597fdc99f4a08eb71079bc61
Matthias Hinz <matthias.m.hinz@gmail.com>
2018-02-14 11:07:14 +0100

    Alter startups script

--- shiny-portals/shiny-server.sh
+++ shiny-portals/shiny-server.sh
@@ -3,5 +3,7 @@
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
-
+# Pre-render data and generate export files
+cd /srv/shiny-server
+Rscript "R/generate_all_data.R"
 exec shiny-server >> /var/log/shiny-server.log 2>&1
