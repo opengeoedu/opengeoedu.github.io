f202d25a807c2750a5c92a3763fb9bf68a99e96a
Matthias Hinz <matthias.m.hinz@gmail.com>
2018-06-22 15:41:08 +0200

    update cookie consent

--- Tests/generate_leaflet_map.R
+++ Tests/generate_leaflet_map.R
@@ -24,9 +24,12 @@ portale <- read.csv("out_geodata/portale_shifted.csv")
 portale$Link <- paste0("<a href=\"",htmlEscape(portale$URL),"\" target=\"_blank\">",htmlEscape(portale$Titel),"</a>")
 portale$popup <- paste0("<a href=\"",htmlEscape(portale$URL),"\" target=\"_blank\">",htmlEscape(portale$Titel),"</a><br>", htmlEscape(portale$Beschreibung),"<p><b>Ort:&nbsp;</b>", htmlEscape(portale$Ort),"</p>")
 portale$label = htmlEscape(paste(portale$Titel, "|", portale$Ort))
-portale$GDI <- portale$GDI == "ja"
-portale$Typ <- as.factor(portale$GDI)
+portale$GDI <- portale$Typ == "gdi"
+
+portale$Typ <- as.factor(portale$GeodatDI)
 levels(portale$Typ) <- c("Open Data Portal","GDI")
+
+leaflet(portale) %>% addMarkers()
 
 source("R/create_map_function.R")
 m <- createMap(portale)
